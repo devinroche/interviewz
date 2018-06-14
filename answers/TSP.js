2d464290e1d30b1aacccb22705ec44c4eda9be41
Devin Roche <Roche.devin@ymail.com>
2018-06-14 16:24:45 -0700

    somewhat working tsp

--- answers/TSP.js
+++ answers/TSP.js
@@ -7,27 +7,49 @@ function TSP(){
         g.addNode(i)
     }
 
-    g.addEdge(1, 2, 4)
-    g.addEdge(1, 3, 6)
-    
-    g.addEdge(2, 4, 3)
-    g.addEdge(3, 5, 2)
+    g.addEdge(1, 2, 10)
+    g.addEdge(1, 3, 15)
+    g.addEdge(1, 4, 20)
 
-    g.addEdge(4, 5, 8)
+    g.addEdge(2, 1, 10)
+    g.addEdge(2, 3, 35)
+    g.addEdge(2, 4, 25)
+
+    g.addEdge(3, 1, 15)
+    g.addEdge(3, 2, 35)
+    g.addEdge(3, 4, 30)
+
+    g.addEdge(4,3,30)
+    g.addEdge(4,1,20)
+    g.addEdge(4,2,25)
 
     let visited = [1]
-    let no_visit = Object.keys(g.vertices).splice(1)
+    let no_visit = Object.keys(g.vertices).map(el => parseInt(el))
 
     let done = false;
     let distance = 0
     let tmp_node = g.vertices[1]
+    let foo;
 
     while(!done){
-        visited.push(tmp_node.node)
-        let foo = findShort(tmp_node)
+        foo = findShort(tmp_node)
+
+        visited.push(foo.node)
+        no_visit = removeEl(no_visit, foo.node)
+
         distance += foo.weight;
-        tmp_node = foo.node
+        tmp_node = g.vertices[foo.node]
+
+        if(visited.length === Object.keys(g.vertices).length+1 && no_visit.length === 0)
+            done = true;
     }
+
+    console.log(visited)
+    console.log(distance)
+}
+
+function removeEl(arr, el) {
+    return arr.filter(e => e !== el);
 }
 
 function findShort(node){
